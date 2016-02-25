939a8a62fd8a0b6b60fcb5b86f19bac61db4f253
Justin Lynn <j@jaesharp.com>
2016-02-25 13:33:46 +1100

    initial build of docker which runs in a semi-production configuration for usage on development workstations

---
+++ spec/vault/vault_spec.rb
@@ -0,0 +1,18 @@
+require 'docker'
+require 'serverspec'
+
+describe "vault" do
+  image = Docker::Image.build_from_dir('.')
+
+  set :os, :family => :alpine
+  set :backend, :docker
+  set :docker_image, image.id
+
+  it "installs the correct version of vault" do
+    expect(vault_version).to include('0.5.0')
+  end
+
+  def vault_version
+    command('/usr/local/bin/vault version').stdout
+  end
+end
